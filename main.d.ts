983031b7db3fa09dbb0d6f9564550efe21095f84
order&chaos <sean@orderandchaoscreative.com>
2023-08-06 18:39:51 +0100

    add typings

---
+++ main.d.ts
@@ -0,0 +1,14 @@
+declare module 'tidyYml' {
+    /**
+     * Tidies the YAML, removing extra whitespace to the left and extra blank lines.
+     *
+     * @param {TemplateStringsArray} strings - Template literal strings.
+     * @param {...any} values - Template literal values.
+     *
+     * @returns {string} - The tidied YAML string.
+     */
+    export default function tidyYml(
+        strings: TemplateStringsArray,
+        ...values: any[]
+    ): string;
+}
